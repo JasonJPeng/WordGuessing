24aaef1aafe3a7502eac7748ea19ce0e52137a2e
Jason Peng <jason.j.peng@gamil.com>
2019-07-14 00:39:38 -0700

    multiple questions

--- index.js
+++ index.js
@@ -2,30 +2,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Word = require("./Word.js");
 
-var targetWord = "Jason Peng (XXX)";
-var maxGuess = targetWord.length*2;
-var iGuess = 0;
-var msgQuestion = "Name of US President or Vice President: "
+var arrPrompts = [
+    {"q": "What's my name ?", "a": "Jason Peng" },
+    {"q": "Who is my father? ", "a": "J. W. Peng"},
+    {"q": "Who is our President? ", "a": "Donald Trump"}    
+]
 
-var guessWord = new Word(targetWord);
-var msgPrompt = {type:"input", name: "char", message: msgQuestion};
+var maxGuess, iGuess, msgPrompt, objWord, targetWord;
+var gameNum = 0;
+function startGuessing() {
+   var i = gameNum % arrPrompts.length;
+   targetWord = arrPrompts[i].a;  
+   maxGuess = targetWord.length*2;
+   iGuess = 0;
+   objWord = new Word(targetWord);
+   msgPrompt = {type:"input", name: "char", message: arrPrompts[i].q};
+   
+   objWord.displayResult();
+   gameNum++;
+}
 
-guessWord.displayResult();
 function enterAChar () {
     inquirer.prompt([msgPrompt]).then( ans => {
        var ch = ans.char.charAt(0);
-       guessWord.guessOne(ch);
-       guessWord.displayResult(); 
-       if (!guessWord.allDone) {
+       objWord.guessOne(ch);
+       objWord.displayResult(); 
+       if (!objWord.allDone) {
          if (iGuess < maxGuess) {
              iGuess++;
              enterAChar ();
          }
         } else {
             console.log("Good Job !");
+            console.log("--------------------");
+            startGuessing();
+            enterAChar();
         }          
     }
     )
 }
 
+startGuessing();
 enterAChar();
